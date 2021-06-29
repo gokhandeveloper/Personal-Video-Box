fb951bc766d2846cf0a7acde99e600a839352a83
gokhandilek <gokhanengineering@gmail.com>
2021-06-29 23:38:10 +0800

    navigation fixed.

--- src/ui/ui-react/src/components/App.js
+++ src/ui/ui-react/src/components/App.js
@@ -1,11 +1,50 @@
-import React, { Component } from 'react';
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import React, {Component, useState} from 'react';
+import {Alert, Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 class App extends Component {
+
     constructor(props) {
         super(props)
+        this.state = {
+            show: 0
+        };
     }
-    render() {
+
+
+
+    async componentDidMount() {
+
+        await this.loadWeb3()
+    }
+
+    async loadWeb3() {
+        if (window.ethereum) {
+    console.log("eth11")
+        }
+        else if (window.web3) {
+
+        }
+        else {
+
+            // window.alert()
+
+
+            if (this.state.show==0) {
+                return (
+                    <Alert variant="danger" onClose={() => this.setState({show:1})}  dismissible>
+                        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                        <p>
+                            Change this and that and try again. Duis mollis, est non commodo
+                            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+                            Cras mattis consectetur purus sit amet fermentum.
+                        </p>
+                    </Alert>
+                );
+            }
+            return <Button onClick={() => this.state.show==0}>Show Alert</Button>;
+        }
+    }
+    render( ) {
         return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Brand href="#home">Video Of the Day</Navbar.Brand>
@@ -13,7 +52,6 @@ class App extends Component {
             <Navbar.Collapse id="responsive-navbar-nav">
             </Navbar.Collapse>
         </Navbar>
-
         );
     }
 
--- src/ui/ui-react/src/index.js
+++ src/ui/ui-react/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
